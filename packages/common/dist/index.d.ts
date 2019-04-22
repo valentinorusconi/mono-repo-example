5305b385a06bacb431469f2e16b3ed28d3f41b45
vaekek <vfrusconi@gmail.com>
2019-04-22 16:39:47 +0200

    :sparkles: setup monorepo

---
+++ packages/common/dist/index.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/emin93/react-native-template-typescript
+ *
+ * @format
+ */
+/// <reference types="react" />
+export declare const App: () => JSX.Element;
